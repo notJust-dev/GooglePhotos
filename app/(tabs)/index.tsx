1a2613305815c68677a882eb38f0913234915f15
Vadim Savin <savinvadim1312@gmail.com>
2024-08-23 18:03:58 +0300

    Load and display Photos from Media Library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,45 @@
+import { Image } from 'expo-image';
+import * as MediaLibrary from 'expo-media-library';
 import { Stack } from 'expo-router';
-import {} from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Text } from 'react-native';
 
 export default function Home() {
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+  const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
+
+  useEffect(() => {
+    if (permissionResponse?.status !== 'granted') {
+      requestPermission();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (permissionResponse?.status === 'granted') {
+      loadLocalAssets();
+    }
+  }, [permissionResponse]);
+
+  const loadLocalAssets = async () => {
+    const assetsPage = await MediaLibrary.getAssetsAsync();
+    console.log(JSON.stringify(assetsPage, null, 2));
+
+    setLocalAssets(assetsPage.assets);
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
+      <FlatList
+        data={localAssets}
+        numColumns={4}
+        contentContainerClassName="gap-[2px]"
+        columnWrapperClassName="gap-[2px]"
+        renderItem={({ item }) => (
+          <Image source={{ uri: item.uri }} style={{ width: '25%', aspectRatio: 1 }} />
+        )}
+      />
     </>
   );
 }
