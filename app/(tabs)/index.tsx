c08bfa57a9b3e4c92114407a16a97449de3027b7
Vadim Savin <savinvadim1312@gmail.com>
2024-08-23 18:19:22 +0300

    Pagination for local media

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,9 @@ export default function Home() {
   const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
 
   const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [endCursor, setEndCursor] = useState<string>();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (permissionResponse?.status !== 'granted') {
@@ -22,10 +25,18 @@ export default function Home() {
   }, [permissionResponse]);
 
   const loadLocalAssets = async () => {
-    const assetsPage = await MediaLibrary.getAssetsAsync();
-    console.log(JSON.stringify(assetsPage, null, 2));
+    if (loading || !hasNextPage) {
+      return;
+    }
+    setLoading(true);
+    const assetsPage = await MediaLibrary.getAssetsAsync({ after: endCursor });
+    // console.log(JSON.stringify(assetsPage, null, 2));
 
-    setLocalAssets(assetsPage.assets);
+    setLocalAssets((existingItems) => [...existingItems, ...assetsPage.assets]);
+
+    setHasNextPage(assetsPage.hasNextPage);
+    setEndCursor(assetsPage.endCursor);
+    setLoading(false);
   };
 
   return (
@@ -36,6 +47,9 @@ export default function Home() {
         numColumns={4}
         contentContainerClassName="gap-[2px]"
         columnWrapperClassName="gap-[2px]"
+        onEndReached={loadLocalAssets}
+        onEndReachedThreshold={1}
+        refreshing={loading}
         renderItem={({ item }) => (
           <Image source={{ uri: item.uri }} style={{ width: '25%', aspectRatio: 1 }} />
         )}
